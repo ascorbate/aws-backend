edff759a328dc7d961e651de280db9e1c59e9f8a
ascorbate <hogdogcar@gmail.com>
2025-02-22 16:22:26 +0100

    feat:add get all products and add stack code

--- lib/aws-backend-stack.ts
+++ lib/aws-backend-stack.ts
@@ -1,16 +1,36 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as apigateway from 'aws-cdk-lib/aws-apigateway';
+import { data } from '../util/util';
 
 export class AwsBackendStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const fetchAllProductsLambda = new lambda.Function(this, 'FetchAllProductsFunction', {
+      runtime: lambda.Runtime.NODEJS_22_X,
+      code: lambda.Code.fromAsset('lambda'),
+      handler: 'getProductsList.handler',
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwsBackendQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+
+    const productCatalogApi = new apigateway.RestApi(this, 'ProductCatalogApi', {
+      restApiName: 'Product Catalog API',
+      description: 'API for managing product catalog',
+      defaultCorsPreflightOptions: {
+        allowOrigins: apigateway.Cors.ALL_ORIGINS,
+        allowMethods: apigateway.Cors.ALL_METHODS,
+        allowHeaders: ['Content-Type', 'Authorization'],
+      },
+    });
+
+    const productsResource = productCatalogApi.root.addResource('products');
+    productsResource.addMethod(
+      'GET',
+      new apigateway.LambdaIntegration(fetchAllProductsLambda)
+    );
+
+   
   }
 }
